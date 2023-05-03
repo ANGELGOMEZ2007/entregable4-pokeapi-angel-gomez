b7b0ee31f97be5d6c8b6e070c56a1e6bf937249e
silverblazer2007 <aagc8969@gmail.com>
2023-05-03 07:26:18 -0400

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,27 @@ import Home from './pages/Home'
 import Pokedex from './pages/Pokedex'
 import ProtectedRoutes from './pages/ProtectedRoutes'
 import PokeInfo from './pages/PokeInfo'
+import Footer from './components/Footer'
+import footer from './components/styles/footer.css'
 
 function App() {
 
   return (
-    <div className="app">
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route element={<ProtectedRoutes/>}>
-          <Route path='/pokedex' element={<Pokedex/>}/>
-          <Route path='/pokedex/:name' element={<PokeInfo />}/>
-        </Route>
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path='/pokedex' element={<Pokedex />} />
+            <Route path='/pokedex/:name' element={<PokeInfo />} />
+          </Route>
+        </Routes>
+
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer__container'>
+        <div className="div__red"></div>
+        <div className="circle">
+            <div className='circle__center'></div>
+        </div>
+        <div className="div__black"></div>
+    </div>
+  )
+}
+
+export default Footer
